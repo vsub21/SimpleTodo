c14a1bab874364f94955994d4e7c674ca77c27e3
vsub21 <viveksub971@gmail.com>
2017-01-05 11:21:31 -0500

    added list functionality

--- app/src/main/java/vsub21/simpletodo/MainActivity.java
+++ app/src/main/java/vsub21/simpletodo/MainActivity.java
@@ -2,12 +2,90 @@ package vsub21.simpletodo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static vsub21.simpletodo.R.id.lvItems;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvItems;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvItems = (ListView)findViewById(R.id.lvItems);
+//        items = new ArrayList<>();
+        readItems();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+
+        // Adapter allows us to easily display the contents of an ArrayList within a ListView
+
+        lvItems.setAdapter(itemsAdapter);
+
+//        items.add("First Item");
+//        items.add("Second Item");
+        setupListViewListener();
+    }
+
+    private void setupListViewListener()
+    {
+        lvItems.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener()
+                {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapter, View item, int pos, long id)
+                    {
+                        items.remove(pos);
+                        itemsAdapter.notifyDataSetChanged();
+                        writeItems();
+                        return true;
+                    }
+                });
+    }
+
+    private void readItems()
+    {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            items = new ArrayList<String>(FileUtils.readLines(todoFile));
+        } catch (IOException e) {
+            items = new ArrayList<String>();
+        }
+    }
+
+    private void writeItems()
+    {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(todoFile, items);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onAddItem(View v)
+    {
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+        writeItems();
     }
 }
